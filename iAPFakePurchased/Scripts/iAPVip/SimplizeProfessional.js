a69d232224227464b34f1e1779b09dae66b35644
TienPD3 <TienPD3@icloud.com>
2023-01-07 14:01:51 +0700

    Updated

--- iAPFakePurchased/Scripts/iAPVip/SimplizeProfessional.js
+++ iAPFakePurchased/Scripts/iAPVip/SimplizeProfessional.js
@@ -1,17 +1,18 @@
 let obj = JSON.parse($response.body);
-obj["membership"]["type"] = "PROFESSIONAL";
 
-obj["features"]["QUY_DAU_TU_QUY_NUOC_NGOAI"] = "1";
-obj["features"]["LOC_CO_PHIEU_GIAO_DICH_NOI_BO"] = "1";
-obj["features"]["LOC_CO_PHIEU_NANG_CAO"] = "1";
-obj["features"]["REWARD_STOCK"] = "1";
-obj["features"]["LOC_CO_PHIEU_BIEN_AN_TOAN"] = "1";
-obj["features"]["PHAN_TICH_GIA_HANG_HOA"] = "1";
-obj["features"]["STOCK_RELATIVE_STRENGTH"] = "1";
-obj["features"]["LIMIT_VIEW_COMPANY"] = "1";
+obj["data"]["membership"]["type"] = "PROFESSIONAL";
 
-obj["membershipStatus"] = true;
+obj["data"]["features"]["QUY_DAU_TU_QUY_NUOC_NGOAI"] = "1";
+obj["data"]["features"]["LOC_CO_PHIEU_GIAO_DICH_NOI_BO"] = "1";
+obj["data"]["features"]["LOC_CO_PHIEU_NANG_CAO"] = "1";
+obj["data"]["features"]["REWARD_STOCK"] = "1";
+obj["data"]["features"]["LOC_CO_PHIEU_BIEN_AN_TOAN"] = "1";
+obj["data"]["features"]["PHAN_TICH_GIA_HANG_HOA"] = "1";
+obj["data"]["features"]["STOCK_RELATIVE_STRENGTH"] = "1";
+obj["data"]["features"]["LIMIT_VIEW_COMPANY"] = "1";
 
-obj["systemMessage"]["visible"] = true;
+obj["data"]["membershipStatus"] = true;
+
+obj["data"]["systemMessage"]["visible"] = true;
 
 $done({body: JSON.stringify(obj)});
