9a5509d24692add4d3a1f7998ffc8666b8f9a018
TienPD3 <TienPD3@icloud.com>
2023-01-21 12:39:42 +0700

    Hack Supper English

--- iAPFakePurchased/Scripts/iAPVip/SupperEnglishPremium.js
+++ iAPFakePurchased/Scripts/iAPVip/SupperEnglishPremium.js
@@ -1,6 +1,6 @@
 var obj = JSON.parse($response.body);
 
 obj.isPremium = true;
-obj.membershipExpiredDate = '9999-12-31T00:00:00.000Z';
+obj.membershipExpiredDate = '9999-12-31';
 
 $done({ body: JSON.stringify(obj) });
--- iAPFakePurchased/Scripts/iAPVip/SupperEnglishPremiumCourses.js
+++ iAPFakePurchased/Scripts/iAPVip/SupperEnglishPremiumCourses.js
@@ -3,7 +3,8 @@ var arrayObj = JSON.parse($response.body);
 arrayObj.forEach(obj => {
     obj.is_premium = true;
     obj.isTrialCourse = true;
-    obj.userCourseExpiredDate = '9999-12-31T00:00:00.000Z';
+    obj.userCourseExpiredDate = '9999-12-31';
+    obj.isEnrolled = 1;
 });
 
 $done({ body: JSON.stringify(arrayObj) });
