20fb146678affcbcd1b2c973f7e5b6c7add07457
TienPD3 <TienPD3@icloud.com>
2023-01-21 12:26:29 +0700

    Hack Supper English

--- iAPFakePurchased/Scripts/iAPVip/SupperEnglishPremiumCourses.js
+++ iAPFakePurchased/Scripts/iAPVip/SupperEnglishPremiumCourses.js
@@ -2,7 +2,7 @@ var arrayObj = JSON.parse($response.body);
 
 arrayObj.forEach(obj => {
     obj.is_premium = true;
-    obj.isTrialCourse = false;
+    obj.isTrialCourse = true;
     obj.userCourseExpiredDate = '9999-12-31T00:00:00.000Z';
 });
 
