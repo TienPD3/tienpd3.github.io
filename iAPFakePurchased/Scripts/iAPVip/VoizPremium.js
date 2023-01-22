3ec71d25157549d8f4aecb3b7b6128865da8c2ec
TienPD3 <TienPD3@icloud.com>
2023-01-22 08:21:19 +0700

    Hack Voiz

--- iAPFakePurchased/Scripts/iAPVip/VoizPremium.js
+++ iAPFakePurchased/Scripts/iAPVip/VoizPremium.js
@@ -1,10 +1,10 @@
 var obj = JSON.parse($response.body);
 
-obj.data.promotion = "vip_reward";
+obj.data.promotion = "vip";
 obj.data.plan_subscriptions = [
       {
         "remaining_day" : 2,
-        "promotion" : "vip_reward",
+        "promotion" : "vip",
         "end_at" : "23:48 24-01-2023",
         "name" : "Gói VIP \u001dquà tặng",
         "start_at" : "23:48 21-01-2023"
