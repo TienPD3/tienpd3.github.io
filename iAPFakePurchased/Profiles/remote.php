cfb98405b39fd32b58fff47b9896dd72115f0af3
TienPD3 <TienPD3@icloud.com>
2023-01-24 21:32:50 +0700

    Fix Quantumnlt X

--- iAPFakePurchased/Profiles/remote.php
+++
@@ -1,3 +0,0 @@
-shadowsocks=127.0.0.1:10086, method=aes-256-gcm, password=.shdqqy2ruAzJK6EAy(A, fast-open=false, udp-relay=false, tag=Fake iAP Server 01
-shadowsocks=127.0.0.1:10086, method=aes-256-gcm, password=.shdqqy2ruAzJK6EAy(A, fast-open=false, udp-relay=false, tag=Fake iAP Server 02
-shadowsocks=127.0.0.1:10086, method=aes-256-gcm, password=.shdqqy2ruAzJK6EAy(A, fast-open=false, udp-relay=false, tag=Fake iAP Server 03
