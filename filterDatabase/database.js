2396926b9415585c8c0ec14792f253a5776f8a1a
TienPD3 <TienPD3@icloud.com>
2023-10-22 08:26:11 +0700

    Init File

---
+++ filterDatabase/database.js
@@ -0,0 +1,25 @@
+
+var database = `
+Class,Age,Sex,Survived
+First Class,Adult,Male,Survived
+First Class,Adult,Male,Survived
+First Class,Adult,Male,Survived
+First Class,Adult,Male,Survived
+First Class,Adult,Male,Survived
+First Class,Adult,Female,Survived
+First Class,Adult,Female,Survived
+First Class,Adult,Female,Survived
+Second Class,Adult,Male,Perished
+Second Class,Adult,Male,Perished
+Second Class,Adult,Male,Perished
+Third Class,Adult,Male,Survived
+Third Class,Adult,Male,Survived
+Third Class,Adult,Male,Survived
+Third Class,Adult,Male,Survived
+Third Class,Adult,Male,Perished
+Third Class,Adult,Male,Perished
+Crew,Adult,Male,Perished
+Crew,Adult,Male,Perished
+Crew,Adult,Female,Survived
+Crew,Adult,Female,Survived
+`
